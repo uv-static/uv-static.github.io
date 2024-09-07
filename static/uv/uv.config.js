3740d8225b03fa7ae9192a5e3d22d980f24585cb
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-09-07 12:04:44 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://uv-static.github.io/static/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
